5d0ee5c9ca1a68b76f49b3e6c41d72b870c8980d
makerling <d7e5fbb0@opayq.com>
2020-04-30 08:13:39 -0500

    checked for existence of folder

--- Code.js
+++ Code.js
@@ -6,20 +6,27 @@ https://yagisanatode.com/2019/04/01/working-with-google-apps-script-in-visual-st
 */
 
 function createFilesInFolder() {
-  //This creates the folder and gets ID of folder
-  //TODO - make sure this foldername doesn't already exist so user can run script many times
-  var folderId = DriveApp.createFolder('NewsGen').getId();
-  Logger.log("folder id is: " + folderId)
-  //This creates a file in the folder
-  var name = 'source'
-  var resource = {
-    title: name,
-    mimeType: MimeType.GOOGLE_SHEETS,
-    parents: [{ id: folderId }]
+  //This checks existence then creates the folder and gets ID of folder
+  var folder = DriveApp.getFoldersByName("NewsGen");
+  try {
+    var folderName = folder.next();
+    Logger.log("folder exists, skipping creation")
   }
-  //Drive class needs Advanced Drive API turned on to make this call work: 
-  //Resources > Advanced Google services ... > Drive API > On
-  var fileJson = Drive.Files.insert(resource)
-  var fileId = fileJson.id
-  Logger.log("fileId is: " + fileId)
+  catch(e) {
+    var folderId = DriveApp.createFolder("NewsGen").getId();
+    Logger.log("folder doesn't exists, creating it now")
+    Logger.log("folder id is: " + folderId)
+    //This creates a file in the folder
+    var name = 'source'
+    var resource = {
+      title: name,
+      mimeType: MimeType.GOOGLE_SHEETS,
+      parents: [{ id: folderId }]
+    }
+    //Drive class needs Advanced Drive API turned on to make this call work: 
+    //Resources > Advanced Google services ... > Drive API > On
+    var fileJson = Drive.Files.insert(resource)
+    var fileId = fileJson.id
+    Logger.log("fileId is: " + fileId)
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NewsGen
 Newsletter Generator
+[![clasp](https://img.shields.io/badge/built%20with-clasp-4285f4.svg)](https://github.com/google/clasp)
